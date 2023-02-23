c451dd309079ff4f36ee90f4401783fb427047f0
tollia <thorvaldur.arnarson@gmail.com>
2023-02-23 23:49:53 +0000

    Binding for Height and Width to enable user input on desired dimensions. These can change as scaling maintains aspect ratio.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,22 +2,9 @@
 using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Drawing;
-using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
+using System.Windows;
 
 
 namespace ImageScaleEXIFWPF {
@@ -26,9 +13,12 @@ namespace ImageScaleEXIFWPF {
     /// </summary>
     public partial class MainWindow : Window {
         private EXIFImage ?exifImage;
+        public int requestedWidth { get; set; } = 768;
+        public int requestedHeight { get; set; } = 768;
 
         public MainWindow() {
             InitializeComponent();
+            DataContext = this;
         }
 
         private void pickFile_Click(object sender, RoutedEventArgs e) {
@@ -55,7 +45,7 @@ namespace ImageScaleEXIFWPF {
             SaveFileDialog saveFileDialog = new SaveFileDialog();
             saveFileDialog.Filter = "JPEG (*.jpg)|*.jpg";
             if (saveFileDialog.ShowDialog() == true) {
-                Bitmap imageToSave = exifImage.AddExif(exifImage.ScaleMaintainAspect(700, 500));
+                Bitmap imageToSave = exifImage.AddExif(exifImage.ScaleMaintainAspect(requestedWidth, requestedHeight));
                 imageToSave.Save(saveFileDialog.FileName, ImageFormat.Jpeg);
             }
         }
