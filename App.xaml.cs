a3f34a4dc9cf494199c137b9747ea7f8663f82da
tollia <thorvaldur.arnarson@gmail.com>
2023-02-24 00:05:57 +0000

    A bit less annoying exception handling.

--- App.xaml.cs
+++ App.xaml.cs
@@ -11,5 +11,9 @@ namespace ImageHandlerEXIFWPF {
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application {
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e) {
+            MessageBox.Show(e.Exception.Message, "Message from your sponsor", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            e.Handled = true;
+        }
     }
 }
