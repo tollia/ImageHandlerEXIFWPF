d727f0a1317767bb0261aa6293aac4b6a955eb47
tollia <thorvaldur.arnarson@gmail.com>
2023-09-01 12:31:04 +0000

    Removed all MetadataExtractor related things

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,20 +1,19 @@
-﻿using ImageHandlerEXIFWPF.Models;
+﻿using ImageMagick;
 using Microsoft.Win32;
 using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Drawing.Imaging;
 using System.Windows;
 
 
-namespace ImageScaleEXIFWPF {
+namespace ImageScaleEXIFWPF
+{
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
-        private EXIFImage ?exifImage;
         public int requestedWidth { get; set; } = 768;
         public int requestedHeight { get; set; } = 768;
+        private MagickImage? magicImage = null;
+
 
         public MainWindow() {
             InitializeComponent();
@@ -24,29 +23,47 @@ namespace ImageScaleEXIFWPF {
         private void pickFile_Click(object sender, RoutedEventArgs e) {
             OpenFileDialog openFileDialog = new OpenFileDialog();
             if (openFileDialog.ShowDialog() == true) {
+                // Proper code examples here:
+                // https://github.com/dlemstra/Magick.NET/tree/main/docs#documentation
+                magicImage = new(openFileDialog.FileName);
+                exifView.ItemsSource = magicImage.GetExifProfile().Values;
                 FileNameTextBox.Text = openFileDialog.FileName;
-                exifImage = new EXIFImage(openFileDialog.FileName);
-                List<MetadataExtractor.Tag> tags = new();
-                foreach (MetadataExtractor.Directory dir in exifImage.MetaDirectories) {
-                    tags.AddRange(dir.Tags);
-                }
-                exifView.ItemsSource = tags;
             } else {
                 FileNameTextBox.Text = String.Empty;
                 exifView.SelectedIndex = 0;
                 exifView.ItemsSource = null;
-                exifImage = null;
+                magicImage = null;
             }
         }
         private void scaleFile_Click(object sender, RoutedEventArgs e) {
-            if (exifImage == null) {
+            if (magicImage == null) {
                 throw new Exception("There is nothing to scale. Please load an image first.");
             }
             SaveFileDialog saveFileDialog = new SaveFileDialog();
             saveFileDialog.Filter = "JPEG (*.jpg)|*.jpg";
-            if (saveFileDialog.ShowDialog() == true) {
-                Bitmap imageToSave = exifImage.AddExif(exifImage.ScaleMaintainAspect(requestedWidth, requestedHeight));
-                imageToSave.Save(saveFileDialog.FileName, ImageFormat.Jpeg);
+            if (saveFileDialog.ShowDialog() == true && magicImage != null) {
+
+                double width = (double)requestedWidth;
+                double height = (double)requestedHeight;
+                double originalAspect = (double)magicImage.Width / (double)magicImage.Height;
+                double requestedAspect = width / height;
+
+                if (originalAspect > requestedAspect) {
+                    height = Math.Round(width * originalAspect);
+                }
+                else {
+                    width = Math.Round(height * originalAspect);
+                }
+                magicImage.Resize((int)width, (int)height);
+                magicImage.Write(saveFileDialog.FileName, MagickFormat.Jpeg);
+                var exif = magicImage.GetExifProfile();
+                foreach(var exifItem in exif.Values)
+                {
+                    var dt = exifItem.DataType;
+                    string s = exifItem.ToString();
+                }
+
+                // Use magicImage.Write(SomeStream) for more flexible handling 
             }
         }
 
